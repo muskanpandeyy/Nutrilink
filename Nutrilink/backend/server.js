a4e0422a1cf9fb5d40a4047d35c6afc1062ebf4b
Muskan pandey <153353499+muskanpandeyy@users.noreply.github.com>
2025-08-24 23:06:41 +0530

    Update server.js

--- Nutrilink/backend/server.js
+++ Nutrilink/backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/nutrilink', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.MONGO_URI,', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const User = mongoose.model('User', new mongoose.Schema({
   username: String,
